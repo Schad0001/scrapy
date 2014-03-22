5b8d0adfa8e993a279f3b64478e8d7603ad98e01
Edgars Vasiljevs <edgars@scandiweb.com>
2014-03-22 18:08:10 +0200

    Code cleanup

--- src/scrapy.js
+++ src/scrapy.js
@@ -3,141 +3,136 @@
 
 var nom = require('nom');
 var async = require('async');
+var sugar = requre('sugar');
 
 var Scrapy = function(options, callback) {
 
-	// this is that
-	var that = this;
+    // this is that
+    var self = this;
 
-	// default options
-	this.options = {
-		concurrency: 1, // simultaneous requests
-		sleep: 1000, // time interval between requests
-		debug: false, // output some information
-	};
+    // default options
+    this.options = {
+        concurrency: 1, // simultaneous requests
+        sleep: 1000, // time interval between requests
+        debug: false // output some information
+    };
 
-	// default loop optionss
-	this._loop_options = {
-		start: 1,
-		end: false,
-		while: false,
-		step: 1,
-	};
+    // default loop options
+    this._loop_options = {
+        start: 1,
+        end: false,
+        while: false,
+        step: 1
+    };
 
-	// override default options
-	for (var attr in options) { 
-		this.options[attr] = options[attr]; 
-	}	
+    // override default options
+    this.options = Object.merge(this.options, options);
 
-	// initialize async queue
-	this._queue = async.queue(function(options, done) {
+    // initialize async queue
+    this._queue = async.queue(function(options, done) {
 
-		that.debug('Downloading:', options.url);
+        self.debug('Downloading:', options.url);
 
-		// read the page
-		nom(options.url, function(err, $) {
+        // load the page
+        nom(options.url, function(err, $) {
 
-			if (err) {
-				throw err;
-			}
+            if (err) throw err;
 
-			// execute a callback
-			options.callback.call({ url: options.url, content : $.html() }, $);
+            // execute a callback
+            options.callback.call({ url: options.url, content : $.html() }, $);
 
-			// wait a little before next request
-			setTimeout(done, that.options.sleep);
+            // wait a little before next request
+            setTimeout(done, self.options.sleep);
+        });
 
-		});	
+    }, this.options.concurrency);
 
-	}, this.options.concurrency);
+    // loop step. executed on every loop cycle
+    this._loop_step = function(url, config, callback) {
 
-	// loop step. executed on every loop cycle
-	this._loop_step = function(url, config, callback) {
+        var stop = false, current = config.current;
 
-		var that = this, stop = false, current = config.current;
+        // set current place
+        config.current += config.step;
 
-		// set current place
-		config.current += config.step;	
+        // add new task to queue
+        this._queue.push({
+            url: url.replace('{%i}', current), // url with replaced iterator
+            callback: function($) {
 
-		// add new task to queue
-		this._queue.push({
-			url: url.replace('{%i}', current), // url with replaced iterator
-			callback: function($) {
-				
-				// check while condition
-				if (config['while'] !== false && current > 1) {
+                // check while condition
+                if (config['while'] !== false && current > 1) {
 
-					// type of while condition
-					var type = typeof config['while'];
+                    // type of while condition
+                    var type = typeof config['while'];
 
-					// check condition depending on while type
-					if (type === 'function' && ! config['while']($, current)) {
-						stop = true;
-					}
-					else if (type === 'object' && ! config['while'].test($.html())) {
-						stop = true;
-					}
-					else if (type === 'string' && ! $(config['while']).length) {
-						stop = true;
-					}
+                    // check condition depending on while type
+                    if (type === 'function' && ! config['while']($, current)) {
+                        stop = true;
+                    }
+                    else if (type === 'object' && ! config['while'].test($.html())) {
+                        stop = true;
+                    }
+                    else if (type === 'string' && ! $(config['while']).length) {
+                        stop = true;
+                    }
 
-				}
+                }
 
-				// check end condition
-				if (config.end !== false && config.current > config.end) {
-					stop = true;
-				}
+                // check end condition
+                if (config.end !== false && config.current > config.end) {
+                    stop = true;
+                }
 
-				// call user loop callback
-				callback.call(this, $, config.current - 1);
+                // call user loop callback
+                callback.call(this, $, config.current - 1);
 
-				// if no conditions are met, continue loop
-				if ( ! stop) {
-					that._loop_step(url, config, callback);
-				}
+                // if no conditions are met, continue loop
+                if ( ! stop) {
+                    self._loop_step(url, config, callback);
+                }
 
-			}
-		});
+            }
+        });
 
-	};
+    };
 
-	// Output debug information
-	this.debug = function() {
-		if (this.options.debug) {
-			console.log(arguments[0], arguments[1]);
-		}
-	};
+    // Output debug information
+    this.debug = function() {
+        if (this.options.debug) {
+            console.log(arguments[0], arguments[1]);
+        }
+    };
 
-	// add page download to queue
-	this.get = function(url, callback) {
-		this._queue.push({
-			url: url, 
-			callback: callback,
-		});
-	};
+    // add page download to queue
+    this.get = function(url, callback) {
+        this._queue.push({
+            url: url,
+            callback: callback
+        });
+    };
 
-	// start a loop with conditions
-	this.loop = function(url, options, callback) {
+    // start a loop with conditions
+    this.loop = function(url, options, callback) {
 
-		// default options
-		var config = this._loop_options;
+        // default options
+        var config = this._loop_options;
 
-		// override user options
-		for (var attr in options) { 
-			config[attr] = options[attr]; 
-		}
+        // override user options
+        for (var attr in options) {
+            config[attr] = options[attr];
+        }
 
-		// set current page to start
-		config.current = config.start;
+        // set current page to start
+        config.current = config.start;
 
-		// start a loop
-		this._loop_step(url, config, callback);
+        // start a loop
+        this._loop_step(url, config, callback);
 
-	};
+    };
 
-	// return this instance
-	return this;
-
+    // return this instance
+    return this;
 };
 
 
--- test/scrapy_test.js
+++ test/scrapy_test.js
@@ -1,4 +1,3 @@
-
 var Scrapy = require("../dist/scrapy");
 var connect = require('connect');
 
@@ -6,63 +5,62 @@ var server = connect.createServer(
     connect.static(__dirname)
 ).listen(8080);
 
-
 module.exports = {
 
-	testObject: function(test) {
+    testObject: function(test) {
 
-		var scrapy = new Scrapy();
+        var scrapy = new Scrapy();
 
-		test.expect(3);
+        test.expect(3);
 
-		test.equal( typeof scrapy , 'object' );
-		test.equal( typeof scrapy.get , 'function' );
-		test.equal( typeof scrapy.loop , 'function' );
+        test.equal( typeof scrapy , 'object' );
+        test.equal( typeof scrapy.get , 'function' );
+        test.equal( typeof scrapy.loop , 'function' );
 
-		test.done();
+        test.done();
 
-	},
+    },
 
-	testGet: function(test) {
+    testGet: function(test) {
 
-		var scrapy = new Scrapy();
+        var scrapy = new Scrapy();
 
-		scrapy.get('http://127.0.0.1:8080/pages/index.html', function($) {
-			test.equal( $('title').text() , 'Success!');
-			test.done();
-		});
+        scrapy.get('http://127.0.0.1:8080/pages/index.html', function($) {
+            test.equal( $('title').text() , 'Success!');
+            test.done();
+        });
 
-	},
+    },
 
-	testLoop: function(test) {
+    testLoop: function(test) {
 
-		var scrapy = new Scrapy();
+        var scrapy = new Scrapy();
 
-		test.expect(6);
+        test.expect(6);
 
-		var ran = 0;
+        var ran = 0;
 
-		scrapy.loop('http://127.0.0.1:8080/pages/page_{%i}.html', { end: 3 }, function($) {
+        scrapy.loop('http://127.0.0.1:8080/pages/page_{%i}.html', { end: 3 }, function($) {
 
-			/_(\d+)\./.test(this.url);
+            /_(\d+)\./.test(this.url);
 
-			var id = RegExp.$1;
+            var id = RegExp.$1;
 
-			test.equal(typeof $, 'function');
-			test.equal( $('title').text() , 'Page ' + id );
-			
-			if (++ran == 3) {
-				test.done();
-			}				
+            test.equal(typeof $, 'function');
+            test.equal( $('title').text() , 'Page ' + id );
 
-		});
+            if (++ran == 3) {
+                test.done();
+            }
 
-	},
+        });
 
-	testExit: function(test) {
-		server.close();
-		test.done();
-	}
-	
+    },
+
+    testExit: function(test) {
+        server.close();
+        test.done();
+    }
+
 }
 
